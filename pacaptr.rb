cae601e5c296a5e67e809b1023704be32d61247e
Rami3L <rami3l@outlook.com>
2020-05-29 15:49:28 +0800

    fix: v0.1.0-alpha.7, take 2

--- pacaptr.rb
+++ pacaptr.rb
@@ -4,13 +4,11 @@ class Pacaptr < Formula
   version "0.1.0-alpha.7"
   url "https://github.com/rami3l/pacaptr/releases/download/v0.1.0-alpha.7/pacaptr-macos-amd64.tar.gz"
   sha256 "823ae8b26750fa516448817d9d0b8c41bf4d9e98007cfe9e60a6180ac16448ec"
-
-  =begin
-  if OS.linux?
-    url "url_goes_here"
-    sha256 "sha256_goes_here"
-  end
-  =end
+  
+  # if OS.linux?
+  #   url "url_goes_here"
+  #   sha256 "sha256_goes_here"
+  # end
 
   def install
     bin.install "pacaptr"
