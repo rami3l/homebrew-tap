c692f3c797c70ebb9a1f3cb8efe8121f5e34aca1
Rami3L <rami3l@outlook.com>
2020-06-02 22:26:00 +0800

    feat: add linuxbrew install support

--- pacaptr.rb
+++ pacaptr.rb
@@ -5,10 +5,10 @@ class Pacaptr < Formula
   url "https://github.com/rami3l/pacaptr/releases/download/v0.1.0-alpha.7/pacaptr-macos-amd64.tar.gz"
   sha256 "823ae8b26750fa516448817d9d0b8c41bf4d9e98007cfe9e60a6180ac16448ec"
   
-  # if OS.linux?
-  #   url "url_goes_here"
-  #   sha256 "sha256_goes_here"
-  # end
+  if OS.linux?
+    url "https://github.com/rami3l/pacaptr/releases/download/v0.1.0-alpha.7/pacaptr-linux-amd64.tar.gz"
+    sha256 "da6a81b8f9523bd10b00a453f63ec8349f841a955c9de5ddbb243b31a568b733"
+  end
 
   def install
     bin.install "pacaptr"
