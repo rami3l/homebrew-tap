53179d7db20deb08269a7690a5b5d9b2d609d1df
Rami3L <rami3l@outlook.com>
2020-06-14 20:31:07 +0800

    feat: v0.2.0-alpha6

--- pacaptr.rb
+++ pacaptr.rb
@@ -1,13 +1,13 @@
 class Pacaptr < Formula
   desc "Pacman syntax wrapper for many package managers"
   homepage "https://github.com/rami3l/pacaptr"
-  version "v0.2.0-alpha.2"
-  url "https://github.com/rami3l/pacaptr/releases/download/v0.2.0-alpha.2/pacaptr-macos-amd64.tar.gz"
-  sha256 "37a704bb05c71fc71699fbdc3b47f84582b9c0248a6cfec22084a4d143d333f2"
+  version "v0.2.0-alpha6"
+  url "https://github.com/rami3l/pacaptr/releases/download/v0.2.0-alpha6/pacaptr-macos-amd64.tar.gz"
+  sha256 "1289a33d2821dd465ff27165c6be959eafe3e145609b4005a10ad04a91e060bb"
   
   if OS.linux?
-    url "https://github.com/rami3l/pacaptr/releases/download/v0.2.0-alpha.2/pacaptr-linux-amd64.tar.gz"
-    sha256 "b84162d89f7ffc79f33d4072daea7ba620f14c089d8071f712c42b2f50385bf7"
+    url "https://github.com/rami3l/pacaptr/releases/download/v0.2.0-alpha6/pacaptr-linux-amd64.tar.gz"
+    sha256 "f631128afb339e4bf0c54ab3bda4c1950175d49c30b63aa344c58510a3c44a43"
   end
 
   def install
