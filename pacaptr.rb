87958485523bed226dcc18dbd97b4221062ce607
rami3l <rami3l@outlook.com>
2020-11-19 11:10:22 +0100

    fix: fix shasum for `pacaptr` v0.8.1

--- pacaptr.rb
+++ pacaptr.rb
@@ -3,11 +3,11 @@ class Pacaptr < Formula
     homepage "https://github.com/rami3l/pacaptr"
     version "v0.8.1"
     url "https://github.com/rami3l/pacaptr/releases/download/v0.8.1/pacaptr-macos-amd64.tar.gz"
-    sha256 "5f1539a871010b7831e4e339aea180e878de6fa4250cc1b7647c6f5286f67925"
+    sha256 "28e1d569666a2e5858b1f082369a41317880083c54b85096f4602bb0d39993e0"
     
     if OS.linux?
       url "https://github.com/rami3l/pacaptr/releases/download/v0.8.1/pacaptr-linux-amd64.tar.gz"
-      sha256 "24e5320f06f13ba8936b2065f9d0368e30039cfc074dee6677b95e98a1e94b50"
+      sha256 "2dd10c34c4df3de486f1253f7ae7b2266e5836f1c6e341db0411e9b94970a70e"
     end
 
     def install
