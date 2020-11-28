2fc0c17a08b4aeadca64ccd19f0df0f21f2fd1ff
rami3l <rami3l@outlook.com>
2020-11-28 17:10:48 +0100

    test: add `affinity-*` test

--- Casks/affinity-designer.rb
+++ Casks/affinity-designer.rb
@@ -1,7 +1,6 @@
 cask "affinity-designer" do
     version "1.8.6"
-    # TODO: sha256
-    # sha256 ""
+    sha256 "68dc8e85a6725f15e8d7336829870d92b1f1e88a27501ba11866dfec328e6c9a"
 
     # s3-eu-west-1.amazonaws.com was verified as official when first introduced to the cask
     url "https://s3-eu-west-1.amazonaws.com/affinity-update/mac/retail/Affinity%20Designer-#{version}.app.zip"
