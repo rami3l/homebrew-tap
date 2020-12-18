eed11706e5bec3f235f6ae694ae2818d14ba021f
rami3l <rami3l@outlook.com>
2020-12-18 13:07:04 +0100

    feat: add `pacaptr` v0.9.0

--- pacaptr.rb
+++ pacaptr.rb
@@ -1,13 +1,13 @@
 class Pacaptr < Formula
     desc "Pacman syntax wrapper for homebrew, chocolatey, apt, and more."
     homepage "https://github.com/rami3l/pacaptr"
-    version "v0.8.1"
-    url "https://github.com/rami3l/pacaptr/releases/download/v0.8.1/pacaptr-macos-amd64.tar.gz"
-    sha256 "28e1d569666a2e5858b1f082369a41317880083c54b85096f4602bb0d39993e0"
+    version "v0.9.0"
+    url "https://github.com/rami3l/pacaptr/releases/download/v0.9.0/pacaptr-macos-amd64.tar.gz"
+    sha256 "8a18641a478d84c901ae5c4c1d04f9630268ec41fe3f8fca01afad7ed0cd5741"
     
     if OS.linux?
-      url "https://github.com/rami3l/pacaptr/releases/download/v0.8.1/pacaptr-linux-amd64.tar.gz"
-      sha256 "2dd10c34c4df3de486f1253f7ae7b2266e5836f1c6e341db0411e9b94970a70e"
+      url "https://github.com/rami3l/pacaptr/releases/download/v0.9.0/pacaptr-linux-amd64.tar.gz"
+      sha256 "7c323b69c8b3cead08a2680663e1b02ff1f0def1f2533072070b1f827a8f3954"
     end
 
     def install
