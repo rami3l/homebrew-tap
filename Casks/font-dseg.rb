0c41707f380294556ff7b143c8847df0cd817075
rami3l <rami3l@outlook.com>
2020-12-27 17:37:59 +0100

    refactor: fix format

--- Casks/font-dseg.rb
+++ Casks/font-dseg.rb
@@ -10,12 +10,12 @@ cask "font-dseg" do
     [
         "DSEG7-Classic/DSEG7Classic",
         "DSEG7-Classic-MINI/DSEG7ClassicMini",
-        "DSEG7-Modern/DSEG7Modern", 
-        "DSEG7-Modern-MINI/DSEG7ModernMini", 
+        "DSEG7-Modern/DSEG7Modern",
+        "DSEG7-Modern-MINI/DSEG7ModernMini",
         "DSEG14-Classic/DSEG14Classic",
         "DSEG14-Classic-MINI/DSEG14ClassicMini",
         "DSEG14-Modern/DSEG14Modern", "DSEG14-Modern-MINI/DSEG14ModernMini"
-    ].product(["Bold" ,"BoldItalic", "Italic", "Light", "LightItalic","Regular"]).map do
+    ].product(["Bold", "BoldItalic", "Italic", "Light", "LightItalic", "Regular"]).map do
         |i| font "fonts-DSEG_v#{version.gsub(".", "")}#{i[0]}-#{i[1]}.ttf"
     end
 end
