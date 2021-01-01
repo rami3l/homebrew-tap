e13cc0c38349793186f20a832e0ad4bb68e758ce
rami3l <rami3l@outlook.com>
2021-01-02 00:27:11 +0100

    fix: fix shasum for `freefilesync` v11.4, take 3

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,11 +1,11 @@
 cask "freefilesync" do
     version "11.4"
-    # sha256 "48a9bf9aa7e79dee55bcda35446153160ed56abc47df36522cfe2a9e27a755e2"
+    sha256 "6a74280615cd8c3a7b434b79daf9036d2c0ca3a345d67f244a82c2a4ba33a3f0"
 
     # We use the mirror below for installation:
     # https://github.com/suschizu/freefilesync-mirror
     # url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
-    url "https://github.com/suschizu/freefilesync-mirror/blob/master/FreeFileSync_#{version}_macOS.zip"
+    url "https://github.com/suschizu/freefilesync-mirror/raw/master/FreeFileSync_#{version}_macOS.zip"
 
     name 'FreeFileSync'
     homepage 'https://www.freefilesync.org/'
