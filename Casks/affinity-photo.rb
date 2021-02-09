f29cfc7f332fcbeac832c669981d2de245a76a64
rami3l <rami3l@outlook.com>
2021-02-09 14:20:51 +0100

    feat: add `affinity-*` v1.9.0

--- Casks/affinity-photo.rb
+++ Casks/affinity-photo.rb
@@ -1,6 +1,6 @@
 cask "affinity-photo" do
-    version "1.8.6"
-    sha256 "cb86ade19725924111af6e593bf8777991c0f8655aca24a3d39fae65ca955b48"
+    version "1.9.0"
+    # sha256 "68dc8e85a6725f15e8d7336829870d92b1f1e88a27501ba11866dfec328e6c9a"
 
     # s3-eu-west-1.amazonaws.com was verified as official when first introduced to the cask
     url "https://s3-eu-west-1.amazonaws.com/affinity-update/mac/retail/Affinity%20Photo-#{version}.app.zip"
--- Casks/affinity-publisher.rb
+++ Casks/affinity-publisher.rb
@@ -1,6 +1,6 @@
 cask "affinity-publisher" do
-    version "1.8.6"
-    sha256 "c9a1f98e8fce119aa9ffc8207e4ba5694989033cb63b688e1a1e82fba163c102"
+    version "1.9.0"
+    # sha256 "68dc8e85a6725f15e8d7336829870d92b1f1e88a27501ba11866dfec328e6c9a"
 
     # s3-eu-west-1.amazonaws.com was verified as official when first introduced to the cask
     url "https://s3-eu-west-1.amazonaws.com/affinity-update/mac/retail/Affinity%20Publisher-#{version}.app.zip"
