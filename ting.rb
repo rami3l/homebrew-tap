31d87bce5346034286550582b17a9539d82a49bb
rami3l <rami3l@outlook.com>
2023-03-28 15:09:50 +0800

    feat: add ting v0.1.7

--- ting.rb
+++ ting.rb
@@ -1,13 +1,13 @@
 class Ting < Formula
     desc "Yet another tcping."
     homepage "https://github.com/rami3l/ting"
-    version "v0.1.7"
+    version "v0.2.1"
     url "https://github.com/rami3l/ting/releases/download/v0.1.7/ting-macos-amd64.tar.gz"
-    sha256 "4cac10eace4976141917a42b85479a2ebdba89539f52223d9aa741dbfd29de42"
+    # sha256 "4cac10eace4976141917a42b85479a2ebdba89539f52223d9aa741dbfd29de42"
 
     if OS.linux?
       url "https://github.com/rami3l/ting/releases/download/v0.1.7/ting-linux-amd64.tar.gz"
-      sha256 "dd60c2bdc5e17630db4e5e5d1a19c9ee9b9ccfad0966cbd055e7854fadab6975"
+      # sha256 "dd60c2bdc5e17630db4e5e5d1a19c9ee9b9ccfad0966cbd055e7854fadab6975"
     end
 
     def install
